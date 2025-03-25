d392df69c8c7851f5bd239d351af1916b122d052
Vinay Jose <59995160+vinay-jose@users.noreply.github.com>
2025-03-25 21:50:19 +0530

    Day 6 structures

---
+++ tutorials/day4/app.py
@@ -0,0 +1,14 @@
+from fasthtml.common import *
+
+app,rt = fast_app()
+
+@rt
+def index():
+    return Titled("Hey you", 
+                Body(
+                    Div("Let's check out FT Tags from fastcore library",
+                        {"class": "uk-h1"})
+                )
+            )
+
+serve()
